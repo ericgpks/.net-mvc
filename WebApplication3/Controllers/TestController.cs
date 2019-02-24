ad1f0a6e7afed9bf5ca29ffe3e257e5c659bc77b
ericgpks <sq.wacbc5.nyc@gmail.com>
2019-02-24 14:31:59 +0900

    [add]new solution

---
+++ WebApplication3/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+using WebApplication3.BC;
+
+namespace WebApplication3.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            Test test = new Test();
+            test.Name = "名前";
+            test.Address = "住所";
+            TestBC bc = new TestBC();
+
+            return View(test);
+        }
+    }
+}
---
+++ WebApplication3/Models/Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Test
+    {
+        string name = string.Empty;
+        string address = string.Empty;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+    }
+}
