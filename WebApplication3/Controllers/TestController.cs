a2295efb9cbc34d2be01142d2e4d0d2d1e893120
ericgpks <sq.wacbc5.nyc@gmail.com>
2019-03-09 12:18:23 +0900

    [add]leetcode1の回答追加

--- WebApplication3/Controllers/TestController.cs
+++ WebApplication3/Controllers/TestController.cs
@@ -17,8 +17,34 @@ namespace WebApplication3.Controllers
             test.Name = "名前";
             test.Address = "住所";
             TestBC bc = new TestBC();
-
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] answer;
+            answer = GetAnswer(nums, target);
             return View(test);
+        }
+        
+        /// <summary>
+        /// lettcode1の問題
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        private int[] GetAnswer(int[] nums, int target)
+        {
+            int[] answer = new int[2];
+            for(int outerCount = 0; outerCount < nums.Length; outerCount++)
+            {
+                for(int innerCount = 0; innerCount < nums.Length; innerCount++)
+                {
+                    if(nums[outerCount] + nums[innerCount] == target)
+                    {
+                        answer[0] = outerCount;
+                        answer[1] = innerCount;
+                        break;
+                    }
+                }
+            }
+            return answer;
         }
     }
 }
